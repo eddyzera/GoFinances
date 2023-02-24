5bb94b62b8fc8408fef29e560447ac5f89942180
eddyzera <esilva644@gmail.com>
2023-02-24 13:22:26 -0300

    chore: start project

---
+++ src/screens/Dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export const Dashboard: React.FunctionComponent = () => {
+  return (
+    <View>
+      <Text>Hello this is my dashboard</Text>
+    </View>
+  )
+}
