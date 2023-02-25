b93c12513fabde449818508dc0793efd3fa25a06
eddyzera <esilva644@gmail.com>
2023-02-25 14:01:15 -0300

    refactor: loading font

--- App.tsx
+++ App.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
+import * as SplashScreen from 'expo-splash-screen'
 import { ThemeProvider } from 'styled-components'
 import { Dashboard } from './src/screens'
 import { customTheme } from './src/global'
 
 export default function App() {
+
+  const [ isLoaded ] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  })
+
+  useEffect(() => {
+    const showSplashScreen = async () => {
+      await SplashScreen.preventAutoHideAsync();
+    };
+    showSplashScreen();
+
+  }, [])
+
+  useEffect(() => {
+    const hideSplashScreen = async () => {
+      await SplashScreen.hideAsync();
+    };
+
+    if (isLoaded) hideSplashScreen();
+  }, [isLoaded]);
+
+
+  if (!isLoaded) return null;
   return (
     <ThemeProvider theme={customTheme}>
       <Dashboard />
